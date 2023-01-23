80c2acb85270ac58aec96d0bb57f15bdaa4ba6ec
Luksona2006 <lukabakuridze12@gmail.com>
2023-01-23 18:46:24 +0400

    detail

--- script.js
+++ script.js
@@ -405,7 +405,7 @@ const transfers = function (e) {
     } else {
         if (amount > 0 && recieverAcc && currentAccount.balance >= amount && recieverAcc.username !== currentAccount.username) {
             popupFunc(popUpTransfers, 'transfer')
-            popUpTransferText.textContent = `TRANSFER ${formatCur(amount, currentAccount.locale, currentAccount.currency)}€ TO ${recieverAcc.owner}(${recieverAcc.username})`
+            popUpTransferText.textContent = `TRANSFER ${formatCur(amount, currentAccount.locale, currentAccount.currency)} TO ${recieverAcc.owner}(${recieverAcc.username})`
         } else {
             popupFunc(popUpError)
             if (recieverAcc === undefined) {
